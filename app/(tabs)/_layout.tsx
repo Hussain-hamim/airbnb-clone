da3e9b96c88b8f56b7edb358e37ee55f2f867b25
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-06-25 00:19:52 +0430

    Refactor tab layout and remove unused TabTwoScreen component; update color constants and improve EditScreenInfo styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,14 +19,8 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
+    <Tabs>
+      {/* <Tabs.Screen
         name="index"
         options={{
           title: 'Tab One',
@@ -53,7 +47,7 @@ export default function TabLayout() {
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
-      />
+      /> */}
     </Tabs>
   );
 }
